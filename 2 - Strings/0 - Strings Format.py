b65f89d32210e49e3f01e50692e20e4ddb412df4
huudafakk <emial@gmx.us>
2022-09-21 17:32:59 +0200

    lab-solutions

--- 2 - Strings/0 - Strings Format.py
+++ 2 - Strings/0 - Strings Format.py
@@ -12,16 +12,30 @@ an "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express o
 See the License for the specific language governing permissions and limitations under the License
 '''
 
-s = "hello"
-x = "Python"
-y = "!!!"
-
-print(s + " Python" + "!!!")
-print((s + ' ') * 3 + " Python" + "!" * 10)
-print("hello %(x)s%(y)s" % {"x": "Python", "y": "!!!"})
-print("hello {0}{1}".format(x, y))
-print("hello {1} {0}".format(x, y))
-print("hello {x}{y}".format(x="Pluto", y="???"))
-print(f"hello {x}{y}")
+# s = "hello"
+# x = "Python"
+# y = "!!!"
+#
+# print(s + " Python" + "!!!")
+# print((s + ' ') * 3 + " Python" + "!" * 10)
+# print("hello %(x)s%(y)s" % {"x": "Python", "y": "!!!"})
+# print("hello {0}{1}".format(x, y))
+# print("hello {1} {0}".format(x, y))
+# print("hello {x}{y}".format(x="Pluto", y="???"))
+# print(f"hello {x}{y}")
 
 # ToDO: Format strings coming from user inputs.
+
+n = str(input("Nome:"))
+c = str(input("Cognome:"))
+
+print(f"{n} {c} é il nome del cliente.")
+
+nome = None
+cognome = None
+
+while not nome:
+    nome = input('\ncome fai di nome? ').replace(' ','')
+while not cognome:
+    cognome = input('\ncome fai di cognome? ').replace(' ','')
+print(f"Ciao, {nome} {cognome}")
