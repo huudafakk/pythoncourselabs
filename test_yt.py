ac1a58a6334a38162021e9ffe731ebff8cfddbee
emial <emial@gmx.us>
2022-09-22 15:56:39 +0200

    lab-solutions

---
+++ test_yt.py
@@ -0,0 +1,33 @@
+#Range:
+#i = 10
+#for x in range(1,11):
+#    print(x)
+#    if x == i:
+#        print("wert ist 10")
+#    else:
+#        print("wert ist nicht 10")
+
+#if
+#x = 10
+#if x < 10:
+#    print(f"{x} ist kleiner als 10")
+#elif x == 10:
+#    print (f"{x} ist gleich 10")
+#else:
+#    print(f"{x} ist groesser als 10")
+
+#while
+#x = int(input("Bitte schreibe eine Zahl: "))
+#x = 1
+#while x < 10:
+#    x = x+1
+#    print(f"{x} ist kleiner gleich 10")
+
+#for
+#x = [1,2,3,4,5,6]
+#y = x
+#
+#for z in x:
+#    print(f"{z}")
+#    if z in y:
+#        print(f"{z}")
